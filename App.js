fee4ecb657c7bf86f519961965e124f394b7017b
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2021-12-19 15:31:44 +0000

    Created Home Screen.

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { HomeScreen } from './src/screens/HomeScreen';
+
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <HomeScreen />
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{
+    flex:1
+  }
 });
---
+++ src/screens/RequestScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RequestScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
